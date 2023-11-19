ea2365785de3d4dfa030a5d3c1b87bb33e3e79ba
Igor_Polyakov <itworldigpo@gmail.com>
2023-11-19 14:36:03 +0200

    add form

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
-
 const reviewsSwiper = document.querySelector('.swiper-reviews');
 
-if(reviewsSwiper){
-    const swiper = new Swiper('.swiper-reviews', {
-        autoHeight: true, 
-        loop: true,
-    
-        pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-        },
-    });
+if (reviewsSwiper) {
+  const swiper = new Swiper('.swiper-reviews', {
+    autoHeight: true,
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 }
 
+let useToScroll = document.querySelector('.scroll-menu__link');
 
-let useToScroll = document.querySelector ('.scroll-menu__link');
+useToScroll.addEventListener('click', setScrollTop);
 
-useToScroll.addEventListener ('click', setScrollTop);
-
-function setScrollTop (top){
-    let intoBlock = document.querySelector ('.page__categories');
-    intoBlock.scrollIntoView({
-        behavior: 'smooth'
-    })
+function setScrollTop(top) {
+  let intoBlock = document.querySelector('.page__categories');
+  intoBlock.scrollIntoView({
+    behavior: 'smooth',
+  });
 }
 
-
+const element = document.getElementById('phone');
+const maskOptions = {
+  mask: '+38 000 000-00-00',
+  lazy: false,
+};
+const mask = new IMask(element, maskOptions);
