028be1bfbd2748163e0372d2428e8cf025b428e1
Igor_Polyakov <itworldigpo@gmail.com>
2023-11-20 22:11:25 +0200

    add sum

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// SWIPER ------------------------------------------------------
+
 const reviewsSwiper = document.querySelector('.swiper-reviews');
 
 if (reviewsSwiper) {
@@ -14,11 +16,14 @@ if (reviewsSwiper) {
 
 const element = document.getElementById('phone');
 const maskOptions = {
-  mask: '+38 000 000-00-00',
+  mask: Number,
   lazy: false,
 };
 
 const mask = new IMask(element, maskOptions);
+// ---------------------------------------------------------------
+
+// SCROLL TO -----------------------------------------------------
 
 let scrollToClock = document.querySelector('.scroll__clock');
 
@@ -57,6 +62,49 @@ function setScrollBuy(top) {
     behavior: 'smooth',
   });
 }
+
+// ---------------------------------------------------------------
+
+// ----Full Information in FORM-----------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(eventner) {
+    eventner.preventDefault();
+
+    let error = formValidate(form);
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+
+      if (input.value === '' || validatePhone(phone) == Number) {
+        formAddError(input);
+        error++;
+      }
+    }
+  }
+  function formAddError(input) {
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+  }
+  function validatePhone(phone) {
+    let re = /^[0-9\s]*$/;
+    re.test(Number);
+  }
+});
+// ---------------------------------------------------------------
+
 // Telegram -----------------------------------------------------
 
 const TOKEN = '6751340227:AAF92vFtCP3tSMQpAOb2wl9su694SQ9AQoY';
@@ -72,26 +120,39 @@ document.querySelector('.form__body').addEventListener('submit', function (e) {
   message += `<b>Телефон: </b> ${this.userphone.value}\n`;
   message += `<b>Нова Пошта: </b> ${this.novaposhta.value}\n`;
   message += `<b>Кількість: </b> ${this.value.value}\n`;
+
   axios
     .post(URI_API, {
       chat_id: CHAT_ID,
       parse_mode: 'html',
       text: message,
     })
+
     .then((res) => {
       this.nameSurname.value = '';
       this.City.value = '';
       this.userphone.value = '';
       this.novaposhta.value = '';
-      this.value.value = '';
-      success.innerHTML = 'Дякую! Замовлення надіслано!';
+      this.value.value = '1';
+      success.innerHTML = 'Дякую! Замовлення отримано!';
       success.style.display = 'block';
     })
     .catch((err) => {
-      console.warn(err);
-    })
-    .finelly(() => {
-      console.log('All Good');
+      console.log(err);
     });
 });
+// ---------------------------------------------------------------
+
+// ---------------Total Value------------------------------------------------
+let select = document.getElementById('select-velue');
+
+select.addEventListener('change', function () {
+  let getValue = this.value;
+  let totalPay = document.querySelector('.value-price');
+  let showPay = getValue * 1200;
+  totalPay.innerHTML = 1200;
+  totalPay.innerHTML = `${showPay}`;
+  console.log(showPay);
+});
+
 // ---------------------------------------------------------------
